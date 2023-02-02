ace40ae1f0632a145fea87fccf1c6de68e53f9aa
marcos.robles <mirh92@outlook.com>
2023-02-01 22:18:41 -0600

    Creating a data model with an interface

---
+++ src/app/data/user-settings.ts
@@ -0,0 +1,8 @@
+export interface UserSettings { 
+    name: string,
+    emailOffers: boolean,
+    interfaceStyle: string,
+    suscriptionType: string,
+    notes:string
+    
+}
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent {
-
+  userSettings: UserSettings = {
+    name: 'Marcos',
+    emailOffers: false,
+    interfaceStyle: 'dark',
+    suscriptionType: 'Annual',
+    notes: 'Here are some notes...'
+  };
 }
