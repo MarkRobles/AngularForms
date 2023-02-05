e87a558e1c9fc25b3fb2e71ee5f893cdf01a8a46
marcos.robles <mirh92@outlook.com>
2023-02-05 14:47:45 -0600

    Http Access Using HttpClient

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { UserSettings } from './user-settings';
@@ -8,10 +9,11 @@ import { UserSettings } from './user-settings';
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  postUserSettingsForm(userSettings: UserSettings) :Observable<UserSettings> { 
-
-    return of(userSettings);
+  postUserSettingsForm(userSettings: UserSettings) :Observable<any> { 
+   // return of(userSettings);
+    //Dummy url generated with https://putsreq.com
+    return this.http.post('https://putsreq.com/sL0AQy5hpZv1xuLjq8a9',userSettings);
   } 
 }
