9520814c781a856ab85e289b1a8c0fd7e321e2c3
marcos.robles <mirh92@outlook.com>
2023-02-05 17:10:08 -0600

    Handling PostErros

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -23,13 +23,31 @@ export class UserSettingsFormComponent {
 
   userSettings: UserSettings = { ...this.originalUserSettings };//Use spread operator to copy values from originalUserSettings, so you always work with a copy of the form values and avoid lost the original if the user press back or cancel button
   //This is used to make a copy of top level properties, if you want a deep copy (objest within objects). You could use Lodash.deepclone
+  postError = false;
+  postErrorMessage = '';
 
+  onHttpError(errorResponse: any) {
+    console.log('error:', errorResponse);
+    this.postError = true;
+    this.postErrorMessage = errorResponse.error.errorMessage;
+    
+  }
   onSubmit(form: NgForm) { 
     console.log('in onSubmit:', form.valid);
-    this.dataService.postUserSettingsForm(this.userSettings).subscribe(
-      result => console.log("Success: ", result),
-      error => console.log("Error: ",error)
-    );
+    console.log('data:', form.value);
+    if (form.valid) {
+
+      
+      this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+        result => console.log("Success: ", result),
+        error => this.onHttpError(error)
+        // error => console.log("Error: ",error)
+      );
+    } else { 
+      this.postError = true;
+      this.postErrorMessage ="Please fix the above errors";
+    }
+
 
   }
 
